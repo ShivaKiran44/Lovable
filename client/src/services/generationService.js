9a1da8e35bc7977265c6d11fa24ac3992699e1c5
ShivaKiran44 <shiva23241a6144@grietcollege.com>
2026-06-05 10:12:56 +0530

    chore: publish client build to gh-pages

--- client/src/services/generationService.js
+++
@@ -1,8 +0,0 @@
-import api from './api.js';
-
-const generateCode = async (projectId, prompt) => {
-  const response = await api.post(`/generate/${projectId}`, { prompt });
-  return response.data.data;
-};
-
-export { generateCode };
--- server/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import authenticate from "../middleware/auth.middleware.js";
-import {registerUser,LoginUser,getUser,logoutUser} from "../controllers/auth.controller.js";
-const router = Router();
-router.post("/register",registerUser);
-router.post("/login",LoginUser);
-router.get("/me",authenticate,getUser);
-router.post("/logout",authenticate,logoutUser);
-export default router;
